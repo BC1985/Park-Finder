d6450f8665a51c04a0ec2864cbc45cd21ef6e618
Ben Cohen <benjcohen1985@gmail.com>
2019-12-03 21:44:29 -0500

    Refactor code to use autocomplete

--- main.js
+++ main.js
@@ -2,22 +2,9 @@
 const apiKey = "sWie0nqphB0xnsS26IndXTwAVp35qjsiB3ElCH8y";
 const searchURL = "https://developer.nps.gov/api/v1/parks";
 
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params).map(
-    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
-  );
-  return queryItems.join("&");
-}
-
-function getParks(stateSearch) {
-  const params = {
-    stateCode: stateSearch,
-    limit: 50,
-    api_key: apiKey
-  };
-
-  const queryString = formatQueryParams(params);
-  const url = searchURL + "?" + queryString;
+function getParks(abbr) {
+  const url = `${searchURL}?stateCode=${abbr}&limit=50&api_Key=${apiKey}`;
+  console.log(url);
   const loadingAnimation = $(".loading");
   fetch(url)
     .then(loadingAnimation.removeClass("hidden"))
@@ -39,14 +26,8 @@ function getParks(stateSearch) {
 function showResults(responseJson) {
   const parkInfo = responseJson.data;
   const stateInput = $("#state-search").val();
-  const stateName = getStateName(stateInput);
-  $("#state").append(
-    `<h2 id="state">      
-        ${capitalizeState(stateName)}</h2>`
-  );
-
+  $("#state").append(`<h2 id="state">${stateInput}</h2>`);
   for (let i = 0; i < parkInfo.length; i++) {
-    // $(".container").hide();
     $(".results").append(
       `<div class="park-container">
       <h2 class='park-name'>${parkInfo[i].fullName}</h2>  
@@ -55,13 +36,14 @@ function showResults(responseJson) {
       <li><a href='${parkInfo[i].directionsUrl}'target="_blank"><i class="fas fa-map-signs icon"></i></a></li> 
       <li><i class="fas fa-cloud-sun icon" onclick = $('.weather-info').toggleClass('hidden')></i></a></li>
       </div>  
-      <div class="weather-info hidden"><p>${parkInfo[i].weatherInfo}</p></div>
+      <div class="weather-info hidden"><p id="weather-info">${parkInfo[i].weatherInfo}</p></div>
       <div class="description"
         <li><p>${parkInfo[i].description}</p></li>
       </div>
         <li><h3><a href='${parkInfo[i].url}' class="link-btn" target="_blank">Website</a></h3></li></div>`
     );
   }
+
   //prepares form for another search
   $(".results").removeClass("hidden");
   $("input[type=text]").val("");
@@ -69,152 +51,149 @@ function showResults(responseJson) {
   $("form").addClass("flex");
 }
 
-function capitalizeState(str) {
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-}
 function watchForm() {
-  $("form").submit(event => {
-    event.preventDefault();
-    let stateSearch = $("#state-search").val();
-    if (validateState(stateSearch)) {
-      getParks(stateSearch);
-      //empties previous search results
-      $("#error-message").empty();
-      $("#state").empty();
-    }
+  $("form").submit(e => {
+    let stateInput = $("#state-search").val();
+    e.preventDefault();
+    getParks(convertRegion(stateInput));
+    //   //empties previous search results
+    $("#error-message").empty();
+    $("#state").empty();
     $(".results").empty();
   });
 }
-function validateState(stateSearch) {
-  const stateAbb = [
-    "al",
-    "ak",
-    "az",
-    "ar",
-    "ca",
-    "co",
-    "ct",
-    "de",
-    "fl",
-    "ga",
-    "hi",
-    "id",
-    "il",
-    "in",
-    "ia",
-    "ks",
-    "ky",
-    "la",
-    "me",
-    "md",
-    "ma",
-    "mi",
-    "mn",
-    "ms",
-    "mo",
-    "mt",
-    "ne",
-    "nv",
-    "nh",
-    "nj",
-    "nm",
-    "ny",
-    "nc",
-    "nd",
-    "oh",
-    "ok",
-    "or",
-    "pa",
-    "ri",
-    "sc",
-    "sd",
-    "tn",
-    "tx",
-    "ut",
-    "vt",
-    "va",
-    "wa",
-    "wv",
-    "wi",
-    "wy"
-  ];
-  if (!stateAbb.includes(stateSearch.toLowerCase())) {
-    $("#error-message").html(
-      "Please make sure you have the correct state abbreviation."
-    );
-    $("input[type=text]").val("");
-    return false;
-  } else {
-    return true;
+// Displays options for autocomplete
+const statesList = [
+  "Alabama",
+  "Alaska",
+  "American Samoa",
+  "Arizona",
+  "Arkensas",
+  "District Of Columbia",
+  "US Virgin Islands",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachussetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Misouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New york",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Puerto Rico",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming"
+];
+$("#state-search").autocomplete({
+  source: statesList,
+  classes: {
+    "ui-autocomplete": "highlight"
   }
-} //make the full state names equivelant to the abbreviations
-const states = {
-  al: "alabama",
-  ak: "alaska",
-  az: "arizona",
-  ar: "arkensas",
-  ca: "california",
-  co: "colorado",
-  ct: "connecticut",
-  de: "delaware",
-  fl: "florida",
-  ga: "georgia",
-  hi: "hawaii",
-  id: "idaho",
-  il: "illinois",
-  in: "indiana",
-  ia: "iowa",
-  ks: "kansas",
-  ky: "kentucky",
-  la: "louisiana",
-  me: "maine",
-  md: "maryland",
-  ma: "massachussetts",
-  mi: "michigan",
-  mn: "minnesota",
-  ms: "mississippi",
-  mo: "misouri",
-  mt: "montana",
-  ne: "nebraska",
-  nv: "nevada",
-  nh: "new hampshire",
-  nj: "new jersey",
-  nm: "new mexico",
-  ny: "new york",
-  nc: "north carolina",
-  nd: "north dakota",
-  oh: "ohio",
-  ok: "oklahoma",
-  or: "oregon",
-  pa: "pennsylvania",
-  ri: "rhode island",
-  sc: "south carolina",
-  sd: "south dacota",
-  tn: "tennessee",
-  tx: "texas",
-  ut: "utah",
-  vt: "vermont",
-  va: "virginia",
-  wa: "washington",
-  wv: "west virginia",
-  wi: "wisconsin",
-  wy: "wyoming"
-};
+});
 
-function getAbbreviation(state) {
-  const _states = Object.keys(states).reduce((acc, current) => {
-    acc[states[current]] = current;
+// Converts states names to abbreivations as they appear in the JSON data
 
-    return acc;
-  }, {});
+function convertRegion(input) {
+  var statesAndAbbreviations = [
+    ["Alabama", "AL"],
+    ["Alaska", "AK"],
+    ["American Samoa", "AS"],
+    ["Arizona", "AZ"],
+    ["Arkansas", "AR"],
+    ["California", "CA"],
+    ["Colorado", "CO"],
+    ["Connecticut", "CT"],
+    ["Delaware", "DE"],
+    ["District Of Columbia", "DC"],
+    ["Florida", "FL"],
+    ["Georgia", "GA"],
+    ["Hawaii", "HI"],
+    ["Idaho", "ID"],
+    ["Illinois", "IL"],
+    ["Indiana", "IN"],
+    ["Iowa", "IA"],
+    ["Kansas", "KS"],
+    ["Kentucky", "KY"],
+    ["Louisiana", "LA"],
+    ["Maine", "ME"],
+    ["Maryland", "MD"],
+    ["Massachusetts", "MA"],
+    ["Michigan", "MI"],
+    ["Minnesota", "MN"],
+    ["Mississippi", "MS"],
+    ["Missouri", "MO"],
+    ["Montana", "MT"],
+    ["Nebraska", "NE"],
+    ["Nevada", "NV"],
+    ["New Hampshire", "NH"],
+    ["New Jersey", "NJ"],
+    ["New Mexico", "NM"],
+    ["New York", "NY"],
+    ["North Carolina", "NC"],
+    ["North Dakota", "ND"],
+    ["US Virgin Islands", "VI"],
+    ["Ohio", "OH"],
+    ["Oklahoma", "OK"],
+    ["Oregon", "OR"],
+    ["Pennsylvania", "PA"],
+    ["Puerto Rico", "PR"],
+    ["Rhode Island", "RI"],
+    ["South Carolina", "SC"],
+    ["South Dakota", "SD"],
+    ["Tennessee", "TN"],
+    ["Texas", "TX"],
+    ["US Virgin Islands", "VI"],
+    ["Utah", "UT"],
+    ["Vermont", "VT"],
+    ["Virginia", "VA"],
+    ["Washington", "WA"],
+    ["West Virginia", "WV"],
+    ["Wisconsin", "WI"],
+    ["Wyoming", "WY"]
+  ];
 
-  return _states[state];
-}
-
-function getStateName(state) {
-  return states[state.toLowerCase()];
+  var i;
+  for (i = 0; i < statesAndAbbreviations.length; i++) {
+    if (statesAndAbbreviations[i][0] == input) {
+      console.log(statesAndAbbreviations[i][1]);
+      return statesAndAbbreviations[i][1];
+    }
+  }
 }
 
 function renderPage() {
